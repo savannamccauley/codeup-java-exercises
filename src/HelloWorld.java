5427a3f71006106d72b030bd790ea46a0eea073c
Savanna McCauley <savannamccauley@yahoo.com>
2023-04-14 16:51:12 -0500

    Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,45 @@
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.print("World !");
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+        String myString = "savanna";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x +=5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y =4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+            x /= y;
+            y -=x;
+                System.out.println(y);
+                System.out.println(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
